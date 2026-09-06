35ed94669040bd05e3468fc9ed165facc533f39f
sashlearn-developingteam <sashlearn.help@gmail.com>
2026-09-06 09:59:45 +0530

    fix: load overlay transparency override

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '@fontsource-variable/inter';
 import './styles.css';
+import './overlay-transparency.css';
 import { App } from './app/App';
 import { DesktopRuntimeRequired } from './components/DesktopRuntimeRequired';
 
